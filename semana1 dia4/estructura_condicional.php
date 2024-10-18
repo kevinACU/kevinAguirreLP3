a615b3d4b2c45e8ecc0b072a473481b8ae0e950d
kevin <gokublackssj66@gmail.com>
2024-10-18 15:20:05 -0300

    Updated

---
+++ semana1 dia4/estructura_condicional.php
@@ -0,0 +1,32 @@
+<?php
+//Condicion 1
+$valor = rand(1, 10);
+echo "El valor sorteado es $valor" . PHP_EOL;
+if ($valor <=5) {
+    echo "Es menor o igual a 5";
+} else {
+    echo "Es mayor a 5";
+}
+//Condicion 2
+echo PHP_EOL;
+$valor = rand (1, 100);
+echo "El valor sorteado es $valor" . PHP_EOL;
+if ($valor <= 9) {
+    echo "Tiene un digito";
+} elseif  ($valor <100) {
+    echo "Tiene 2 digitos";
+} else {
+    echo "Tiene 3 digitos";
+}
+//condicion 3
+echo PHP_EOL;
+$valor = rand(1, 100);
+echo "El valor sorteado es $valor" . PHP_EOL;
+if ($valor <= 9) {
+    echo "Tiene un digito";
+} elseif  ($valor <100) {
+    echo "Tiene 2 digitos";
+} else {
+    echo "Tiene 3 digitos";
+}
+?>
---
+++ semana1 dia4/tarea_variables.php
@@ -0,0 +1,17 @@
+<?php
+$dia = 24; //se declara una variable del tipo integer
+$sueldo = 758.43; //se declara una variable de tipo double
+$nombre = "utic"; //se declara una variable de tipo string
+$exite = true ; //se declara una variable de tipo boolean
+echo "Variable entera:";
+echo $dia;
+echo PHP_EOL;;
+echo "Variable double:";
+echo $sueldo;
+echo PHP_EOL;
+echo "Variable string:";
+echo $nombre;
+echo PHP_EOL;
+echo "Variable boolean:";
+echo $exite;
+?>
